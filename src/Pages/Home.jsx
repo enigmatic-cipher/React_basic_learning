581897cc3cbe1dc72b14c537f557ba9ee839bf03
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-07-21 19:45:18 +0530

    first_react

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+    <>
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quam obcaecati aliquid eveniet explicabo labore rem quis. Adipisci delectus facere iure illum. Corrupti, inventore incidunt blanditiis delectus soluta animi nulla.</p>
+    </>
+    )
+}
+
+export default Home;
